c35e47d600fbfb2114f6e203de9d7d1b23658c4c
needle3108 <92887393+needle3108@users.noreply.github.com>
2024-12-02 13:12:56 +0100

    Init commit

---
+++ backend/src/main/java/com/net/app/neo4jproject/Neo4jprojectApplication.java
@@ -0,0 +1,13 @@
+package com.net.app.neo4jproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jprojectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/net/app/neo4jproject/Neo4jprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.net.app.neo4jproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Neo4jprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
